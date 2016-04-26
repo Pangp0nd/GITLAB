f4a492ba16769dcee1ab6e9760100fb0358c0996
apinanin <apinand.ain@gmail.com>
2016-04-27 01:27:00 +0700

    edit lottery & create program#3

--- Lottery.c
+++ Lottery.c
@@ -8,7 +8,7 @@ void main()
   
   int YourLotteryNo;  
    
-  printf("Enter your Lottery Number (0 to end): "); 
+  printf("Edit by ain"); 
   scanf("%d", &YourLotteryNo); 
   while (YourLotteryNo != 0) 
   { 
